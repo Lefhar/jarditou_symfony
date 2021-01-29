8d33f398201e74e9045a2973efdbe2ae42226f36
harold <s.lefebvre907@laposte.net>
2021-01-29 11:15:37 +0100

    exercice symfony

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Produits;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ProduitsController extends AbstractController
 {
     /**
@@ -13,8 +13,11 @@ class ProduitsController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Produits::class);
+        $produits = $repo->findAll();
         return $this->render('produits/index.html.twig', [
             'controller_name' => 'ProduitsController',
+            'produits'=> $produits
         ]);
     }
 }
